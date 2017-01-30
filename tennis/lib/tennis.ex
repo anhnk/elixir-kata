67793c3a55fcd047772dc3810e913283530fd256
Anh Do <dotuanhoanganh@gmail.com>
2017-01-30 13:23:03 +1100

    First commit

---
+++ tennis/lib/tennis.ex
@@ -0,0 +1,18 @@
+defmodule Tennis do
+  @moduledoc """
+  Documentation for Tennis.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tennis.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
