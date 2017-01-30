e3e0f8e5b982576313c4844bb349cd0b4ac9a12e
Anh Do <dotuanhoanganh@gmail.com>
2017-01-30 15:49:13 +1100

    Tennis kata 1st take

--- tennis/lib/tennis.ex
+++ tennis/lib/tennis.ex
@@ -1,18 +1,20 @@
 defmodule Tennis do
-  @moduledoc """
-  Documentation for Tennis.
-  """
+  @tennis_score ["Love", "15", "30", "40"]
 
-  @doc """
-  Hello world.
+  def scores(pts, pts) when pts > 2, do: "Deuce"
+  def scores(pts, pts), do: score(pts) <> " all"
 
-  ## Examples
+  def scores(p1_pts, 0) when p1_pts < 4, do: score(p1_pts) <> " love"
+  def scores(0, p2_pts) when p2_pts < 4, do: "Love " <> score(p2_pts)
 
-      iex> Tennis.hello
-      :world
+  def scores(4, p2_pts) when p2_pts < 3, do: "Game player 1"
+  def scores(p1_pts, 4) when p1_pts < 3, do: "Game player 2"
+  def scores(p1_pts, p2_pts) when (p2_pts >= 3) and (p1_pts - p2_pts == 2), do: "Game player 1"
+  def scores(p1_pts, p2_pts) when (p1_pts >= 3) and (p1_pts - p2_pts == -2), do: "Game player 2"
 
-  """
-  def hello do
-    :world
-  end
+  def scores(p1_pts, p2_pts) when p1_pts - p2_pts == 1, do: "Advantage player 1"
+  def scores(p1_pts, p2_pts) when p1_pts - p2_pts == -1, do: "Advantage player 2"
+
+
+  defp score(pts), do: Enum.at(@tennis_score, pts)
 end
--- tennis/test/tennis_test.exs
+++ tennis/test/tennis_test.exs
@@ -2,7 +2,52 @@ defmodule TennisTest do
   use ExUnit.Case
   doctest Tennis
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "something all" do
+    assert Tennis.scores(0, 0) == "Love all"
+    assert Tennis.scores(1, 1) == "15 all"
+    assert Tennis.scores(2, 2) == "30 all"
+  end
+
+  test "deuce" do
+    assert Tennis.scores(3, 3) == "Deuce"
+    assert Tennis.scores(4, 4) == "Deuce"
+    assert Tennis.scores(5, 5) == "Deuce"
+    assert Tennis.scores(12, 12) == "Deuce"
+  end
+
+  test "something love" do
+    assert Tennis.scores(1, 0) == "15 love"
+    assert Tennis.scores(2, 0) == "30 love"
+    assert Tennis.scores(3, 0) == "40 love"
+  end
+
+  test "love something" do
+    assert Tennis.scores(0, 1) == "Love 15"
+    assert Tennis.scores(0, 2) == "Love 30"
+    assert Tennis.scores(0, 3) == "Love 40"
+  end
+
+  test "game" do
+    assert Tennis.scores(4, 0) == "Game player 1"
+    assert Tennis.scores(4, 1) == "Game player 1"
+    assert Tennis.scores(4, 2) == "Game player 1"
+    assert Tennis.scores(5, 3) == "Game player 1"
+    assert Tennis.scores(12, 10) == "Game player 1"
+
+    assert Tennis.scores(0, 4) == "Game player 2"
+    assert Tennis.scores(1, 4) == "Game player 2"
+    assert Tennis.scores(2, 4) == "Game player 2"
+    assert Tennis.scores(3, 5) == "Game player 2"
+    assert Tennis.scores(10, 12) == "Game player 2"
+  end
+
+  test "advantage" do
+    assert Tennis.scores(4, 3) == "Advantage player 1"
+    assert Tennis.scores(5, 4) == "Advantage player 1"
+    assert Tennis.scores(12, 11) == "Advantage player 1"
+
+    assert Tennis.scores(3, 4) == "Advantage player 2"
+    assert Tennis.scores(4, 5) == "Advantage player 2"
+    assert Tennis.scores(11, 12) == "Advantage player 2"
   end
 end
