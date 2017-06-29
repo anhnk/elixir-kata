6e75fc5d715ce493d738248e9f653b7bf7374409
Anh Do <dotuanhoanganh@gmail.com>
2017-06-29 16:35:20 +1000

    Move some code from script to Simulator module

--- toy_robot/lib/simulator.ex
+++ toy_robot/lib/simulator.ex
@@ -1,11 +1,29 @@
 defmodule Simulator do
+  @usage """
+  ------------------------------------ USAGE ------------------------------------
+  Command:         mix run toy_robot.exs [FILE_PATH]
+  Notes:           The sequence of commands must be on the first line of the file.
+                   Any other lines after it will be ignored.
+  """
+  @wrong_number_of_args_error "\nWrong number of arguments\n\n#{@usage}"
   @initial_state {nil, nil, nil, "REPORT:\n"}     # {x, y, direction, report}
 
-  def run(commands), do: @initial_state |> execute(commands) |> get_report
+  def run([]), do: @wrong_number_of_args_error
+  def run(args) when length(args) > 1, do: @wrong_number_of_args_error
+  def run(filename) do
+    case read_file_and_execute(filename) do
+      {:ok, report} -> report
+      {:error, _} -> "Can't read file"
+    end
+  end
 
+  defp read_file_and_execute(filename) do
+    with {:ok, contents}   <- File.read(filename),
+         commands          <- CommandHelper.normalize(contents),
+         {_, _, _, report} <- execute(@initial_state, commands),
+         do: {:ok, report}
+  end
 
   defp execute(state, []), do: state
   defp execute(state, [command | rest]), do: Robot.apply(state, command) |> execute(rest)
-
-  defp get_report({_, _, _, report}), do: report
 end
--- toy_robot/toy_robot.exs
+++ toy_robot/toy_robot.exs
@@ -1,22 +1,3 @@
-usage = """
------------------------------------- USAGE ------------------------------------
-Command:         mix run toy_robot.exs [FILE_PATH]
-Notes:           The sequence of commands must be on the first line of the file.
-                 Any other lines after it will be ignored.
-"""
-
-case length(System.argv) do
-  1 ->
-    [filename | _] = System.argv
-
-    case File.read(filename) do
-      {:ok, contents} ->
-        contents
-        |> CommandHelper.normalize
-        |> Simulator.run
-        |> IO.puts
-      {:error, :enoent} -> IO.puts "\nFile not found\n\n#{usage}"
-      {:error, _} -> IO.puts usage
-    end
-  _ -> IO.puts "\nWrong number of arguments\n\n#{usage}"
-end
+System.argv
+|> Simulator.run
+|> IO.puts
