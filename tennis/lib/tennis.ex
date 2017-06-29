38aaf1e22b504bd2ac75b4dd23109bcb124a8792
Anh Do <dotuanhoanganh@gmail.com>
2017-06-29 15:05:00 +1000

    Create macro to use in guard

--- tennis/lib/tennis.ex
+++ tennis/lib/tennis.ex
@@ -1,4 +1,12 @@
+defmodule PointSystem do
+  defmacro won_by_2_points(winner_points, loser_points) do
+    quote do: (unquote(loser_points) >= 3) and (unquote(winner_points) - unquote(loser_points) == 2)
+  end
+end
+
 defmodule Tennis do
+  import PointSystem
+
   @tennis_score ["Love", "15", "30", "40"]
 
   def scores(pts, pts) when pts > 2, do: "Deuce"
@@ -9,8 +17,8 @@ defmodule Tennis do
 
   def scores(4, p2_pts) when p2_pts < 3, do: "Game player 1"
   def scores(p1_pts, 4) when p1_pts < 3, do: "Game player 2"
-  def scores(p1_pts, p2_pts) when (p2_pts >= 3) and (p1_pts - p2_pts == 2), do: "Game player 1"
-  def scores(p1_pts, p2_pts) when (p1_pts >= 3) and (p1_pts - p2_pts == -2), do: "Game player 2"
+  def scores(p1_pts, p2_pts) when won_by_2_points(p1_pts, p2_pts), do: "Game player 1"
+  def scores(p1_pts, p2_pts) when won_by_2_points(p2_pts, p1_pts), do: "Game player 2"
 
   def scores(p1_pts, p2_pts) when p1_pts - p2_pts == 1, do: "Advantage player 1"
   def scores(p1_pts, p2_pts) when p1_pts - p2_pts == -1, do: "Advantage player 2"
